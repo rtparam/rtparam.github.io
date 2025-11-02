5761a9cf0a333cf2409ef5e722b4a47fd2830f22
rtparam <aarathiparameswaran@gmail.com>
2025-11-02 01:31:21 +0000

    Deploying to gh-pages from @ rtparam/rtparam.github.io@8d8eb8dcd2059df3b1af0b0f8ba31430cd7c4bc6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "rtparam/aarathiparam.github.io",
+    "data-repo": "rtparam/rtparam.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
