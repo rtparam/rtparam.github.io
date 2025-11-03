f7f5438ea8d138c6a5c09da4ef455fa0e1222dd8
rtparam <aarathiparameswaran@gmail.com>
2025-11-03 04:00:17 +0000

    Deploying to gh-pages from @ rtparam/rtparam.github.io@846755bf508e3a9a28d4ddd12fe418d47a2921cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -467,6 +467,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/aarathip", "_blank");
         },
       },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0005-8255-2152", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
