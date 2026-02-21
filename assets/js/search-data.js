d0801beeff0f8f75a2ecfed34a227153568e40de
rtparam <aarathiparameswaran@gmail.com>
2026-02-21 02:00:44 +0000

    Deploying to gh-pages from @ rtparam/rtparam.github.io@dc98bd575d03605e7ad341b672060c13dc4b4d98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,11 +58,21 @@ ninja.data = [{
           section: "News",},{id: "news-i-started-my-ph-d-in-informatics-at-indiana-univeristy-bloomington",
           title: 'I started my Ph.D. in Informatics at Indiana Univeristy Bloomington!',
           description: "",
-          section: "News",},{id: "projects-prediction-markets-as-a-complex-adaptive-system",
-          title: 'Prediction Markets as a Complex Adaptive System',
-          description: "Final Paper for Advanced Seminar on Complex Systems",
+          section: "News",},{id: "projects-prediction-markets-as-complex-adaptive-systems",
+          title: 'Prediction Markets as Complex Adaptive Systems',
+          description: "Final Paper for Advanced Seminar on Complex Systems that has resulted in an ongoing collaboration to use hyper neural networks and agent-based modelling to study behavior on prediction markets.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-bridge-criticality-in-a-spatial-road-network",
+          title: 'Bridge Criticality in a Spatial Road Network',
+          description: "Ongoing research with Prof. Filippo Radicchi at Indiana University",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
